59f7f11c8b9292da0aeac86134065587c812a0ff
Jaron D Evans <jarondevans@gmail.com>
2018-07-09 15:53:23 +0000

    first comment

---
+++ printBuffer.js
@@ -0,0 +1,44 @@
+function getAndPrintHTML () {
+
+var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  /* Add your code here */
+
+  // notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+https.get(requestOptions, function (response) {
+
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    // console.log('Chunk Received. Length:', data);
+    // console.log("data!!!!" + data)
+    let newData = data;
+    console.log("NewData", newData);
+    // console.log()
+    // let newDat = data();
+  });
+
+// console.log(http.data);
+
+// console.log(newDat);
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  // response.on('end', function() {
+  //   console.log('Response stream complete.');
+  // });
+
+});
+
+}
+
+getAndPrintHTML();
